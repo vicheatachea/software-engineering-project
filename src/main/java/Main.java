a31d75f171cbf6b982ad044ccd07020deaae1dea
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-06 17:58:46 +0200

    Implement proper MVC architecture
    Each part of the project is now fully separated from the others and comments were added to indicate how MVC should be maintained

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import view.View;
+
+/*
+* Main initialises view which initialises controller which initialises model
+* Two-way data binding is not supported due to this schema and should not be needed
+* Creating everything here and passing it down does not seem to be possible
+*/
 public class Main {
+    public static void main(String[] args) {
+        View.launch(View.class);
+    }
 }
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,15 @@
 package controller;
 
+import model.Model;
+
+/*
+* The controller should not manipulate the view directly to maintain separation of concerns
+* The view should be able to call the controller to manipulate the model and retrieve data
+*/
 public class Controller {
+    private final Model model;
+
+    public Controller() {
+        this.model = new Model();
+    }
 }
