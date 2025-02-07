6d91d85c235f215eb42cad93218ad18d57b3e7a4
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-07 17:21:39 +0200

    Add initial UI structure of main page

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,19 +1,24 @@
 package view;
 
-import controller.Controller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/*
+* The controller should be initialized in the main page view controller
+*/
 public class View extends Application {
-    Controller controller;
-
     @Override
     public void start(Stage stage) {
-        // View implementation through FXML
-    }
-
-    @Override
-    public void init() {
-        controller = new Controller();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/pages/main-page.fxml"));
+        try {
+            stage.setScene(new Scene(loader.load()));
+            stage.setTitle("Student Timetable Management System");
+            stage.setMaximized(true);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/view/controllers/MainPageController.java
@@ -0,0 +1,15 @@
+package view.controllers;
+
+import controller.Controller;
+
+/*
+* The controller of the application is initialized here
+* The controller should be passed to other view controllers as needed
+*/
+public class MainPageController {
+    Controller controller;
+
+    public MainPageController() {
+        this.controller = new Controller();
+    }
+}
