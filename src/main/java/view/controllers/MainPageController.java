45b5bacad93017b50c6618b6ec5ed6493a1d87dc
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-08 20:19:39 +0200

    Add logic to allow for multiple views in the main page

--- src/main/java/view/controllers/MainPageController.java
+++ src/main/java/view/controllers/MainPageController.java
@@ -1,15 +1,34 @@
 package view.controllers;
 
 import controller.Controller;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /*
 * The controller of the application is initialized here
 * The controller should be passed to other view controllers as needed
 */
-public class MainPageController {
+public class MainPageController implements Initializable {
     Controller controller;
+    @FXML
+    private SidebarController sidebarController;
+    @FXML
+    private StackPane mainContent;
 
     public MainPageController() {
         this.controller = new Controller();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sidebarController.currentViewProperty().addListener((observableValue, oldValue, newValue) -> {
+            switch (newValue) {
+                // Implement the logic for switching views here
+            }
+        });
     }
 }
--- src/main/java/view/controllers/SidebarController.java
+++ src/main/java/view/controllers/SidebarController.java
@@ -1,4 +1,23 @@
 package view.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import view.components.SidebarButton;
+
 public class SidebarController {
+    private final StringProperty currentView = new SimpleStringProperty();
+    @FXML
+    private VBox sidebar;
+
+    public StringProperty currentViewProperty() {
+        return currentView;
+    }
+
+    private void addButton(String text) {
+        SidebarButton button = new SidebarButton(text);
+
+        sidebar.getChildren().add(button);
+    }
 }
