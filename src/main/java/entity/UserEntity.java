49ab74e465cce3f6ecd38ac7e190fd5520122703
Saku Heinonen <sakuhei@metropolia.fi>
2025-02-10 00:29:07 +0200

    feat: Add entity class for User.   - fields for userId, firstName, lastName, dateOfBirth, socialNumber, role, timetable, groups, and teachingSessions.

---
+++ src/main/java/entity/UserEntity.java
@@ -0,0 +1,107 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
+
+	@Column(nullable = false)
+	private Date dateOfBirth;
+
+	@Column(nullable = false)
+	private String socialNumber;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@OneToOne(optional = false)
+	@JoinColumn(name = "timetable_id", nullable = false)
+	private TimetableEntity timetable;
+
+	@ManyToMany
+	@JoinTable(name = "belongs_to",
+	           joinColumns = @JoinColumn(name = "user_id"),
+	           inverseJoinColumns = @JoinColumn(name = "group_id")
+	)
+	private Set<UserGroupEntity> groups;
+
+	@ManyToMany
+	@JoinTable(name = "teaches",
+	           joinColumns = @JoinColumn(name = "user_id"),
+	           inverseJoinColumns = @JoinColumn(name = "teaching_session_id")
+	)
+	private Set<TeachingSessionEntity> teachingSessions;
+
+	public UserEntity() {
+	}
+
+	public UserEntity(String firstName, String lastName, Date dateOfBirth, String socialNumber, Role role) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.socialNumber = socialNumber;
+		this.role = role;
+	}
+
+	public void setId(Long id) {
+		this.userId = id;
+	}
+
+	public Long getId() {
+		return userId;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setSocialNumber(String socialNumber) {
+		this.socialNumber = socialNumber;
+	}
+
+	public String getSocialNumber() {
+		return socialNumber;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+}
