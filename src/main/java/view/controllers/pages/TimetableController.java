062a7daea967bb0980f138846910d7b5752fa212
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-11 18:09:21 +0200

    Add timetable grid

--- src/main/java/view/controllers/pages/TimetableController.java
+++ src/main/java/view/controllers/pages/TimetableController.java
@@ -1,4 +1,9 @@
 package view.controllers.pages;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class TimetableController {
+    @FXML
+    private GridPane timetableGrid;
 }
