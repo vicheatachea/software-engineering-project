602b371f3c852c82ed48566cdaeca547cd5d5975
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-18 14:35:07 +0200

    Add logic to DatePicker and update date and week labels

--- src/main/java/view/controllers/pages/main/TimetableController.java
+++ src/main/java/view/controllers/pages/main/TimetableController.java
@@ -1,8 +1,9 @@
 package view.controllers.pages.main;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -10,10 +11,18 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.WeekFields;
 
 public class TimetableController {
     private static final int NUMBER_OF_BUTTONS = 2;
+
+    LocalDate currentDate;
+    LocalDate startDate;
+    LocalDate endDate;
+    int currentWeek;
 
     @FXML
     private HBox topbar;
@@ -22,11 +31,14 @@ public class TimetableController {
     @FXML
     private Label dateLabel;
     @FXML
+    private DatePicker datePicker;
+    @FXML
     private Label weekLabel;
 
     @FXML
     private void initialize() {
         addButtons();
+        Platform.runLater(() -> datePicker.setValue(LocalDate.now()));
     }
 
     private void addButtons() {
@@ -39,13 +51,39 @@ public class TimetableController {
             buttons[0].setText("New Event");
             buttons[1].setText("\uD83D\uDD27");
 
-            DatePicker datePicker = new DatePicker();
+            buttons[0].setOnAction(event -> handleAddEvent());
+            // Settings buttons is not yet implemented
 
             topbar.getChildren().addFirst(buttons[0]);
-            topbar.getChildren().addAll(datePicker, buttons[1]);
+            topbar.getChildren().add(buttons[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void handleDatePick() {
+        LocalDate newDate = datePicker.getValue();
+
+        if (newDate == null) {
+            return;
+        }
+
+        currentDate = newDate;
+        startDate = newDate.with(DayOfWeek.MONDAY);
+        endDate = newDate.with(DayOfWeek.SUNDAY);
+        currentWeek = newDate.get(WeekFields.ISO.weekOfWeekBasedYear());
+
+        // Date formatting needs to be updated
+        dateLabel.setText(startDate + " - " + endDate);
+        weekLabel.setText("Week " + currentWeek);
+    }
+
+    private void handleAddEvent() {
+        // Get the start and end times, type, etc
+        // Based on the start and end times allocate the necessary amount of cells
+        // Change margins so that the times match the hours in relation to cell size
+        // Maybe make a separate label component for this
     }
 
     // Not to be implemented yet
@@ -62,12 +100,5 @@ public class TimetableController {
     // Fetch data for an entire week
     private void loadTimetable() {
 
-    }
-
-    private void addEvent() {
-        // Get the start and end times, type, etc
-        // Based on the start and end times allocate the necessary amount of cells
-        // Change margins so that the times match the hours in relation to cell size
-        // Maybe make a separate label component for this
     }
 }
