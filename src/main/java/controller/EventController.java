18a7b39bf2d4cf1ed468c91b676e82a086358c58
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-27 10:19:02 +0200

    Add EventController methods

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import dto.Event;
+import dto.GroupDTO;
 import dto.TeachingSessionDTO;
 import model.EventModel;
 
@@ -15,5 +16,37 @@ public class EventController {
     public List<Event> fetchEventsByUser(LocalDate startDate, LocalDate endDate) {
         // Placeholder
         return new ArrayList<>();
+    }
+
+    // Add an event for a user
+    public void addEventForUser(Event event) {
+        // Placeholder
+    }
+
+    // Update an event for a user
+    public void updateEventForUser(Event oldEvent, Event newEvent) {
+        // Placeholder
+    }
+
+    // Delete an event for a user
+    public void deleteEventForUser(Event event) {
+        // Placeholder
+    }
+
+    // NOTE: Group is currently a String but can be changed to GroupDTO
+
+    // Add an event for a group
+    public void addEventForGroup(Event event, String groupName) {
+        // Placeholder
+    }
+
+    // Update an event for a group
+    public void updateEventForGroup(Event oldEvent, Event newEvent, String groupName) {
+        // Placeholder
+    }
+
+    // Delete an event for a group
+    public void deleteEventForGroup(Event event, String groupName) {
+        // Placeholder
     }
 }
