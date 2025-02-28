9806648ffe11b971cfe0eb7076cc44dfb9118a43
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-28 18:18:54 +0200

    Add isUserLoggedIn() method to UserController

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -4,4 +4,9 @@ import model.UserModel;
 
 public class UserController {
     private final UserModel userModel = new UserModel();
+
+    public boolean isUserLoggedIn() {
+        // Placeholder
+        return false;
+    }
 }
