ef96198afc325a4ef0dfb509304cb1caca3d002c
Saku Heinonen <sakuhei@metropolia.fi>
2025-03-01 00:02:33 +0200

    refactor: add method to fetch all locations in LocationModel

--- src/main/java/model/LocationModel.java
+++ src/main/java/model/LocationModel.java
@@ -1,4 +1,13 @@
 package model;
 
+import dto.LocationDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocationModel {
+	public List<LocationDTO> fetchAllLocations() {
+		// Placeholder
+		return new ArrayList<>();
+	}
 }
