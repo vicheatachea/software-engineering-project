b0afdcd7b631118b1845566fcdc4fa5c98643de6
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-02-28 18:18:31 +0200

    Remove Main class from package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
-package main;
-
 import view.View;
 
 /*
-* main.Main initializes view which initializes controller which initializes model
+* Main initializes view which initializes controller which initializes model
 * Two-way data binding is not supported due to this schema and should not be needed
 * Creating everything here and passing it down does not seem to be possible
 */
