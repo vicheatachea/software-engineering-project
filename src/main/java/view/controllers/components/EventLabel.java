8491eb414b24b2917f7e30f1b3c51e6e732c56b2
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-03-03 09:37:45 +0200

    Add size and offset variable and methods to EventLabel

--- src/main/java/view/controllers/components/EventLabel.java
+++ src/main/java/view/controllers/components/EventLabel.java
@@ -6,14 +6,38 @@ import javafx.scene.control.Label;
 public class EventLabel extends Label {
     private Event event;
 
-    public EventLabel(Event event) {
+    // All the following values represent percentages of the cell size
+    private final double height;
+    private final double top_offset;
+    private int labelPosition = 1;
+    private int numberOfLabels = 1;
+
+    public EventLabel(Event event, double height, double top_offset) {
         // Placeholder
         super("Event");
         this.event = event;
         this.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        this.height = height;
+        this.top_offset = top_offset;
     }
 
     public Event getEvent() {
         return event;
+    }
+
+    public void updateLabelHeight(double cellHeight) {
+        this.setMaxHeight(cellHeight * height);
+        this.setTranslateY(cellHeight * top_offset);
+    }
+
+    public void updateLabelWidth(double cellWidth) {
+        this.setMaxWidth((double) labelPosition / numberOfLabels * cellWidth);
+        this.setTranslateX((double) (labelPosition - 1) / numberOfLabels * cellWidth);
+    }
+
+    public void updateLabelPosition(int labelPosition, int numberOfLabels) {
+        this.labelPosition = labelPosition;
+        this.numberOfLabels = numberOfLabels;
     }
 }
