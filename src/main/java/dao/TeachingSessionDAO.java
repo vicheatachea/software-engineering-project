098381e881aa07d566c6f907d039f1ebd66ed6d9
Saku Heinonen <sakuhei@metropolia.fi>
2025-03-04 23:21:39 +0200

    feat: refactor TeachingSessionDAO to simplify persistence and update methods, changing LocalDateTime to Timestamp

--- src/main/java/dao/TeachingSessionDAO.java
+++ src/main/java/dao/TeachingSessionDAO.java
@@ -4,7 +4,7 @@ import entity.TeachingSessionEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class TeachingSessionDAO {
@@ -15,11 +15,23 @@ public class TeachingSessionDAO {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		try {
-			if (teachingSession.getId() == null || findById(teachingSession.getId()) == null) {
-				em.persist(teachingSession);
-			} else {
-				em.merge(teachingSession);
+			em.persist(teachingSession);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			throw e;
+		} finally {
+			if (em.isOpen()) {
+				em.close();
 			}
+		}
+	}
+
+	public void update(TeachingSessionEntity entity) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		try {
+			em.merge(entity);
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			em.getTransaction().rollback();
@@ -150,14 +162,15 @@ public class TeachingSessionDAO {
 		}
 	}
 
-	public List<TeachingSessionEntity> findAllByTimetableIdDuringPeriod(Long id, LocalDateTime start,
-	                                                                    LocalDateTime end) {
+	public List<TeachingSessionEntity> findAllByTimetableIdDuringPeriod(Long id, Timestamp start,
+	                                                                    Timestamp end) {
 		EntityManager em = emf.createEntityManager();
 		try {
 			return em.createQuery("SELECT t FROM TeachingSessionEntity t WHERE t.timetable.id = :id AND t.StartDate " +
 			                      "BETWEEN :start AND :end", TeachingSessionEntity.class).setParameter("id", id)
 			         .setParameter("start", start).setParameter("end", end).getResultList();
 		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
 			return null;
 		} finally {
 			if (em.isOpen()) {
@@ -165,4 +178,6 @@ public class TeachingSessionDAO {
 			}
 		}
 	}
+
+
 }
