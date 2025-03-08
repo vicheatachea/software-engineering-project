d7884794bd0772f361121f062c4ea821584e134e
Saku Heinonen <sakuhei@metropolia.fi>
2025-03-08 17:34:46 +0200

    feat(EventControllerTest, GroupControllerTest, SubjectControllerTest): update tests to use student username instead of ID

--- src/test/java/controller/GroupControllerTest.java
+++ src/test/java/controller/GroupControllerTest.java
@@ -109,7 +109,6 @@ class GroupControllerTest {
 
 		userController.registerUser(student);
 		userController.authenticateUser(student.username(), student.password());
-		long studentId = userController.fetchCurrentUserId();
 		userController.logout();
 
 		userController.registerUser(teacher);
@@ -125,8 +124,8 @@ class GroupControllerTest {
 		GroupDTO group2 = new GroupDTO("Group2", "TST2", 10, teacherId, subject1.code());
 		groupController.addGroup(group2);
 
-		groupController.addStudentToGroup(group1, studentId);
-		groupController.addStudentToGroup(group2, studentId);
+		groupController.addStudentToGroup(group1, student.username());
+		groupController.addStudentToGroup(group2, student.username());
 
 		userController.logout();
 		userController.authenticateUser(student.username(), student.password());
@@ -179,10 +178,9 @@ class GroupControllerTest {
 		for (UserDTO student : students) {
 			userController.registerUser(student);
 			userController.authenticateUser(student.username(), student.password());
-			long studentId = userController.fetchCurrentUserId();
 			userController.logout();
 			userController.authenticateUser(teacher.username(), teacher.password());
-			groupController.addStudentToGroup(group, studentId);
+			groupController.addStudentToGroup(group, student.username());
 			userController.logout();
 		}
 
@@ -198,7 +196,6 @@ class GroupControllerTest {
 
 		userController.registerUser(student);
 		userController.authenticateUser(student.username(), student.password());
-		long studentId = userController.fetchCurrentUserId();
 		userController.logout();
 
 		userController.registerUser(teacher);
@@ -214,8 +211,8 @@ class GroupControllerTest {
 		GroupDTO group2 = new GroupDTO("Group2", "TST2", 10, teacherId, subject1.code());
 		groupController.addGroup(group2);
 
-		groupController.addStudentToGroup(group1, studentId);
-		groupController.addStudentToGroup(group2, studentId);
+		groupController.addStudentToGroup(group1, student.username());
+		groupController.addStudentToGroup(group2, student.username());
 
 		assertTrue(groupController.isUserGroupOwner("Group1"));
 		assertTrue(groupController.isUserGroupOwner("Group2"));
@@ -291,7 +288,6 @@ class GroupControllerTest {
 
 		userController.registerUser(student);
 		userController.authenticateUser(student.username(), student.password());
-		long studentId = userController.fetchCurrentUserId();
 		userController.logout();
 
 		userController.registerUser(teacher);
@@ -304,7 +300,7 @@ class GroupControllerTest {
 		GroupDTO group1 = new GroupDTO("Group1", "TST1", 10, teacherId, subject1.code());
 		groupController.addGroup(group1);
 
-		groupController.addStudentToGroup(group1, studentId);
+		groupController.addStudentToGroup(group1, student.username());
 
 		userController.logout();
 		userController.authenticateUser(student.username(), student.password());
@@ -320,7 +316,6 @@ class GroupControllerTest {
 
 		userController.registerUser(student);
 		userController.authenticateUser(student.username(), student.password());
-		long studentId = userController.fetchCurrentUserId();
 		userController.logout();
 
 		userController.registerUser(teacher);
@@ -333,7 +328,7 @@ class GroupControllerTest {
 		GroupDTO group1 = new GroupDTO("Group1", "TST1", 10, teacherId, subject1.code());
 		groupController.addGroup(group1);
 
-		groupController.addStudentToGroup(group1, studentId);
+		groupController.addStudentToGroup(group1, student.username());
 		userController.logout();
 
 		userController.authenticateUser(student.username(), student.password());
@@ -344,7 +339,7 @@ class GroupControllerTest {
 		userController.logout();
 		userController.authenticateUser(teacher.username(), teacher.password());
 
-		groupController.removeStudentFromGroup(group1, studentId);
+		groupController.removeStudentFromGroup(group1, student.username());
 
 		userController.logout();
 		userController.authenticateUser(student.username(), student.password());
