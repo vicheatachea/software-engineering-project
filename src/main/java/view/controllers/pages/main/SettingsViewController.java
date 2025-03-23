69dbcf5c50026954f3eda4ca4150de10c335c464
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-03-23 11:57:41 +0200

    Add language selection logic to Settings view controller

--- src/main/java/view/controllers/pages/main/SettingsViewController.java
+++ src/main/java/view/controllers/pages/main/SettingsViewController.java
@@ -1,4 +1,49 @@
 package view.controllers.pages.main;
 
-public class SettingsViewController {
+import controller.BaseController;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import util.LocaleUtil;
+import view.controllers.ControllerAware;
+
+import java.util.List;
+import java.util.Locale;
+
+public class SettingsViewController implements ControllerAware {
+    private BaseController baseController;
+
+    @FXML
+    private ComboBox<String> languageComboBox;
+
+    @FXML
+    private void initialize() {
+        List<Locale> locales = LocaleUtil.getAvailableLocales();
+        locales.forEach(locale ->
+                languageComboBox.getItems().add(locale.getDisplayLanguage(locale))
+        );
+        languageComboBox.addEventHandler(ActionEvent.ACTION, event -> handleLanguageChange());
+
+        Platform.runLater(() -> {
+            Locale currentLocale = Locale.forLanguageTag(baseController.getLocale());
+            languageComboBox.setValue(currentLocale.getDisplayLanguage(currentLocale));
+        });
+    }
+
+    @Override
+    public void setBaseController(BaseController baseController) {
+        this.baseController = baseController;
+    }
+
+    private void handleLanguageChange() {
+        String selectedLanguage = languageComboBox.getValue();
+
+        for (Locale locale : LocaleUtil.getAvailableLocales()) {
+            if (locale.getDisplayLanguage(locale).equals(selectedLanguage)) {
+                baseController.setLocale(locale.toLanguageTag());
+                return;
+            }
+        }
+    }
 }
