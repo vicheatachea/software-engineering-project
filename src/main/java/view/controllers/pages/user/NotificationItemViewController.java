87b527a5010a784fd753c40df8e7b0fdb9c253cb
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-04-01 08:25:18 +0300

    Update NotificationItem to properly display notifications

--- src/main/java/view/controllers/pages/user/NotificationItemViewController.java
+++ src/main/java/view/controllers/pages/user/NotificationItemViewController.java
@@ -1,6 +1,5 @@
 package view.controllers.pages.user;
 
-import controller.UserController;
 import dto.AssignmentDTO;
 import dto.Event;
 import dto.TeachingSessionDTO;
@@ -8,38 +7,34 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import controller.BaseController;
+import view.controllers.ControllerAware;
+
+import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
-public class NotificationItemViewController {
+public class NotificationItemViewController implements ControllerAware {
+    private ResourceBundle viewText;
+    private NotificationsViewController parentController;
+
     @FXML
     private Label eventLabel;
-
-
     @FXML
     private HBox notificationItem;
-    private ResourceBundle viewText;
 
-    private NotificationsViewController parentController;
-    private BaseController baseController;
-    private UserController userController;
-
+    @Override
     public void setBaseController(BaseController baseController) {
-        this.baseController = baseController;
-        this.userController = baseController.getUserController();
         this.viewText = baseController.getLocaleController().getUIBundle();
     }
 
-    public void setNotificationData(NotificationsViewController parent, Event event, int time, ResourceBundle viewText) {
+    public void setNotificationData(NotificationsViewController parent, Event event, int time) {
         this.parentController = parent;
 
         if (event instanceof AssignmentDTO assignmentDTO) {
-            eventLabel.setText(String.format(viewText.getString("notificationItem.assignmentText"), assignmentDTO.assignmentName(), time));
+            eventLabel.setText(MessageFormat.format(viewText.getString("notificationItem.assignmentText"), assignmentDTO.assignmentName(), time));
         } else if (event instanceof TeachingSessionDTO teachingSessionDTO) {
-            eventLabel.setText(String.format(viewText.getString("notificationItem.classOfText"), teachingSessionDTO.subjectCode(), time));
+            eventLabel.setText(MessageFormat.format(viewText.getString("notificationItem.classOfText"), teachingSessionDTO.subjectCode(), time));
         }
     }
-
-
 
     @FXML
     private void handleDelete() {
