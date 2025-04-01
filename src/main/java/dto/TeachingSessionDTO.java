4e1491ae31d9290055f48a51cfe945ae11933301
SakuJuuH <sakuhei@metropolia.fi>
2025-04-01 13:38:39 +0300

    feat(teaching-session): add localeCode to TeachingSessionEntity, DTO, and implement findAllByLocaleDuringPeriod method

--- src/main/java/dto/TeachingSessionDTO.java
+++ src/main/java/dto/TeachingSessionDTO.java
@@ -3,5 +3,5 @@ package dto;
 import java.time.LocalDateTime;
 
 public record TeachingSessionDTO(Long id, LocalDateTime startDate, LocalDateTime endDate, String locationName,
-                                 String subjectCode, String description, long timetableId) implements Event {
+                                 String subjectCode, String description, long timetableId, String localeCode) implements Event {
 }
