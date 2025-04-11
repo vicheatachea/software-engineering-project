f29840da1ca09f0ba824a344b5cd6e8ceb8984eb
SakuJuuH <sakuhei@metropolia.fi>
2025-04-11 17:24:40 +0300

    fix(UserPreferences): Set default locale as en-US

--- src/main/java/model/UserPreferences.java
+++ src/main/java/model/UserPreferences.java
@@ -2,18 +2,16 @@ package model;
 
 import entity.Role;
 
-import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 public final class UserPreferences {
-	private UserPreferences() {
-		// Prevent instantiation
-	}
-
 	private static final String USER_ID_KEY = "user_id";
 	private static final String ROLE = "role";
 	private static final String LOCALE = "locale";
 	private static final Preferences preferences = Preferences.userNodeForPackage(UserPreferences.class);
+	private UserPreferences() {
+		// Prevent instantiation
+	}
 
 	// Store the user ID
 	public static void setUser(long userId, Role role) {
@@ -37,13 +35,13 @@ public final class UserPreferences {
 		preferences.remove(ROLE);
 	}
 
-	// Store the locale
-	public static void setLocale(String locale) {
-		preferences.put(LOCALE, locale);
-	}
-
 	// Retrieve the locale
 	public static String getLocale() {
 		return preferences.get(LOCALE, "en-US");
+	}
+
+	// Store the locale
+	public static void setLocale(String locale) {
+		preferences.put(LOCALE, locale);
 	}
 }
