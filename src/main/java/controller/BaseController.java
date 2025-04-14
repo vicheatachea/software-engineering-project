8c1f6889506757b0ca7344e5676f48532167c91e
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2025-04-14 14:23:24 +0300

    Update BaseController to not return references to mutable objects

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -1,46 +1,36 @@
 package controller;
 
-import model.UserPreferences;
-
 /*
 * The controller should not manipulate the view directly to maintain separation of concerns
 * The view controllers should be able to call each controller to manipulate the model and retrieve data
 * The base controller should be passed among the views until the specific controllers are needed
 */
 public class BaseController {
-    private final EventController eventController = new EventController();
-    private final GroupController groupController = new GroupController();
-    private final LocationController locationController = new LocationController();
-    private final SubjectController subjectController = new SubjectController();
-    private final TimetableController timetableController = new TimetableController();
-    private final UserController userController = new UserController();
-    private final LocaleController localeController = new LocaleController();
-
     public EventController getEventController() {
-        return eventController;
+        return new EventController();
     }
 
     public GroupController getGroupController() {
-        return groupController;
+        return new GroupController();
     }
 
     public LocationController getLocationController() {
-        return locationController;
+        return new LocationController();
     }
 
     public SubjectController getSubjectController() {
-        return subjectController;
+        return new SubjectController();
     }
 
     public TimetableController getTimetableController() {
-        return timetableController;
+        return new TimetableController();
     }
 
     public UserController getUserController() {
-        return userController;
+        return new UserController();
     }
 
     public LocaleController getLocaleController() {
-        return localeController;
+        return new LocaleController();
     }
 }
