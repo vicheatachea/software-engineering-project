74a341e9da0abcfb11aad9bd9342ca5c06d97cc8
SakuJuuH <sakuhei@metropolia.fi>
2025-04-15 15:51:18 +0300

    refactor(MainPageViewController): enhance error handling with logging and improve database connection termination

--- src/main/java/view/controllers/pages/main/MainPageViewController.java
+++ src/main/java/view/controllers/pages/main/MainPageViewController.java
@@ -1,6 +1,7 @@
 package view.controllers.pages.main;
 
 import controller.BaseController;
+import datasource.MariaDBConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,17 +10,20 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import view.controllers.ControllerAware;
 import view.controllers.components.SidebarViewController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class MainPageViewController implements Initializable {
+	private static final Logger logger = LoggerFactory.getLogger(MainPageViewController.class);
 	Stage stage;
 	BaseController baseController;
-
 	@FXML
 	private SidebarViewController sidebarController;
 	@FXML
@@ -33,87 +37,92 @@ public class MainPageViewController implements Initializable {
 
 	public void setStage(Stage stage) {
 		this.stage = stage;
-        stage.setOnCloseRequest(event -> sidebarController.shutdownNotifications());
+		stage.setOnCloseRequest(event -> sidebarController.shutdownNotifications());
 	}
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        SplitPane.setResizableWithParent(sidebar, false);
-        SplitPane.setResizableWithParent(mainContent, false);
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		SplitPane.setResizableWithParent(sidebar, false);
+		SplitPane.setResizableWithParent(mainContent, false);
 
 		sidebarController.setBaseController(baseController);
 
-        sidebarController.currentViewProperty().addListener((observableValue, oldValue, newValue) -> {
-            switch (newValue) {
-                case "home":
-                    loadContent("/layouts/pages/main/home.fxml", null);
-                    break;
-                case "timetable":
-                    loadContent("/layouts/pages/main/timetable.fxml", null);
-                    break;
-                case "groups":
-                    loadContent("/layouts/pages/main/general-page.fxml", "groups");
-                    break;
-                case "subjects":
-                    loadContent("/layouts/pages/main/general-page.fxml", "subjects");
-                    break;
-                case "locations":
-                    loadContent("/layouts/pages/main/general-page.fxml", "locations");
-                    break;
-                case "settings":
-                    loadContent("/layouts/pages/main/settings.fxml", null);
-                    break;
-                case "clear":
-                    break;
-                case "quit":
-                    sidebarController.shutdownNotifications();
-                    stage.close();
-                    break;
-                default:
-                    System.out.println("MainPageController initialize() could not load " + newValue);
-                    break;
-            }
-        });
-    }
-
-    private void loadContent(String fxmlFilePath, String name) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFilePath));
-
-            if (name != null) {
-                switch (name) {
-                    case "groups":
-                        fxmlLoader.setController(new GroupsViewController());
-                        break;
-                    case "subjects":
-                        fxmlLoader.setController(new SubjectsViewController());
-                        break;
-                    case "locations":
-                        fxmlLoader.setController(new LocationsViewController());
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            Node content = fxmlLoader.load();
-            if (content == null) {
-                System.out.println("MainPageController loadContent() could not load " + fxmlFilePath);
-                return;
-            }
-
-            Object subController = fxmlLoader.getController();
-
-			if (subController instanceof ControllerAware) {
-				((ControllerAware) subController).setBaseController(baseController);
+		sidebarController.currentViewProperty().addListener((observableValue, oldValue, newValue) -> {
+			switch (newValue) {
+				case "home":
+					loadContent("/layouts/pages/main/home.fxml", null);
+					break;
+				case "timetable":
+					loadContent("/layouts/pages/main/timetable.fxml", null);
+					break;
+				case "groups":
+					loadContent("/layouts/pages/main/general-page.fxml", "groups");
+					break;
+				case "subjects":
+					loadContent("/layouts/pages/main/general-page.fxml", "subjects");
+					break;
+				case "locations":
+					loadContent("/layouts/pages/main/general-page.fxml", "locations");
+					break;
+				case "settings":
+					loadContent("/layouts/pages/main/settings.fxml", null);
+					break;
+				case "clear":
+					break;
+				case "quit":
+					sidebarController.shutdownNotifications();
+					try {
+						MariaDBConnection.getInstance().terminate();
+					} catch (SQLException e) {
+						logger.error("Error closing database connection: {}", e.getMessage());
+					}
+					stage.close();
+					break;
+				default:
+					logger.info("MainPageController initialize() could not load {} ", newValue);
+					break;
 			}
-            if (subController instanceof SettingsViewController) {
-                ((SettingsViewController) subController).setSidebarViewController(sidebarController);
-            }
+		});
+	}
+
+	private void loadContent(String fxmlFilePath, String name) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFilePath));
+
+			if (name != null) {
+				switch (name) {
+					case "groups":
+						fxmlLoader.setController(new GroupsViewController());
+						break;
+					case "subjects":
+						fxmlLoader.setController(new SubjectsViewController());
+						break;
+					case "locations":
+						fxmlLoader.setController(new LocationsViewController());
+						break;
+					default:
+						break;
+				}
+			}
+
+			Node content = fxmlLoader.load();
+			if (content == null) {
+				logger.info("MainPageController loadContent() could not load {}", fxmlFilePath);
+				return;
+			}
+
+			Object subController = fxmlLoader.getController();
+
+			if (subController instanceof ControllerAware controllerAware) {
+				controllerAware.setBaseController(baseController);
+			}
+			if (subController instanceof SettingsViewController settingsViewController) {
+				settingsViewController.setSidebarViewController(sidebarController);
+			}
 
 			mainContent.getChildren().setAll(content);
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.error("Error loading FXML file: {}", e.getMessage());
 		}
 	}
 }
