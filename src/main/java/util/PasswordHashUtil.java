c9e0a1c9b3d09d52f6af926cc6a18526921c72b3
SakuJuuH <sakuhei@metropolia.fi>
2025-04-15 15:50:36 +0300

    refactor(PasswordHashUtil): enhance error handling with logging and prevent instantiation

--- src/main/java/util/PasswordHashUtil.java
+++ src/main/java/util/PasswordHashUtil.java
@@ -1,11 +1,20 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class PasswordHashUtil {
+
+	private static final Logger logger = LoggerFactory.getLogger(PasswordHashUtil.class);
+
+	private PasswordHashUtil() {
+		// Prevent instantiation
+	}
 
 	public static String generateSalt() {
 		SecureRandom random = new SecureRandom();
@@ -21,12 +30,14 @@ public class PasswordHashUtil {
 			byte[] hashedPassword = md.digest(password.getBytes());
 			return Base64.getEncoder().encodeToString(hashedPassword);
 		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
+			logger.error(e.getMessage());
+			return null;
 		}
 	}
 
 	public static Boolean verifyPassword(String inputPassword, String storedHash, String salt) {
 		String hashedInputPassword = hashPassword(inputPassword, salt);
+		assert hashedInputPassword != null;
 		return hashedInputPassword.equals(storedHash);
 	}
 }
