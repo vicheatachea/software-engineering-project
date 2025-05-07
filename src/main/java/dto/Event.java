cb5fa5833e4917bc60c7584a7cf75788222dfe09
SakuJuuH <sakuhei@metropolia.fi>
2025-05-07 14:32:38 +0300

    docs(event): add javadoc for Event interface

--- src/main/java/dto/Event.java
+++ src/main/java/dto/Event.java
@@ -1,4 +1,12 @@
 package dto;
 
+/**
+ * Marker interface for timetable events in the STMS application.
+ * <p>
+ * Implemented by:
+ * {@link AssignmentDTO}
+ * {@link TeachingSessionDTO}
+ */
+
 public interface Event {
 }
